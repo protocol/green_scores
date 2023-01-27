2e0637783c9ca1a19f736efbcb1accd4932b9400
_earthtospace <niema.widaha@gmail.com>
2023-01-26 20:27:10 -0800

    routing & responsive navigation

---
+++ dcent_audit-results/src/pages/PageCTA.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageCTA = () => {
+  return (
+      <h1>PAGE CTA</h1>
+  )
+}
+
+export default PageCTA
---
+++ dcent_audit-results/src/pages/PageThree.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageThree = () => {
+  return (
+    <h1>
+        PAGE THREE
+    </h1>
+  )
+}
+
+export default PageThree

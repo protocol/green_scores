b6bc7d7d26b4aff8399412497a7c4794b174da90
_earthtospace <niema.widaha@gmail.com>
2023-03-14 16:21:30 -0700

    responsive piknik

--- src/components/SPAssetResultCard.js
+++ src/components/SPAssetResultCard.js
@@ -9,15 +9,15 @@ const SPAssetResultCard = ({asset}) => {
                     Object.keys(asset).map((key, i) => (
                         key === "Renewable Electricity Purchased Documentation" ? 
                             (
-                            <div className="flex p-1 bg-white border border-black border-dotted" key={i}>
-                                <span className='p-1 text-black text-xs dark:text-green-500'>{key} </span>
+                            <div className="flex p-1 bg-white border border-black border-dotted dark:bg-gray-800 dark:border-green-500" key={i}>
+                                <span className='p-1 text-black text-xs dark:text-green-400 '>{key} </span>
                                 <span className='overflow-x-auto flex-wrap p-1 ml-5 text-green-600 text-xs font-semibold dark:text-green-500'>You can download the PDFs here</span>
                             </div>
                             )
                         : 
                         (
-                            <div className="flex p-1 bg-white border border-black border-dotted" key={i}>
-                                <span className='p-1 text-black text-xs dark:text-green-500'>{key} </span>
+                            <div className="flex p-1 bg-white border border-black border-dotted dark:bg-gray-800 dark:border-green-500" key={i}>
+                                <span className='p-1 text-black text-xs dark:text-green-400'>{key} </span>
                                 <span className='overflow-x-auto p-1 ml-5 text-green-600 text-xs font-semibold dark:text-green-500'>{asset[key]}</span>
                             </div>
                         ) 
